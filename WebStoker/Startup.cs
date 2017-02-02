526e8a69d59e959326cbfacff7f5b16000b49cea
Dech <Dech@DECH-PC>
2017-02-02 20:03:35 +0100

    Add project files.

---
+++ WebStoker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebStoker.Startup))]
+namespace WebStoker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
