acd925e4a42178bee89c172ada6c9a585aac52f9
Dech <Dech@DECH-PC>
2017-02-08 21:10:52 +0100

    Test string i jimmiTestclass

--- WebStoker/jimmiTestclass.cs
+++ WebStoker/jimmiTestclass.cs
@@ -8,5 +8,8 @@ namespace WebStoker
     public class jimmiTestclass
     {
         string hejDennis = "Kan du hente det her ned";
+
+        string hejJimmi = "Yes og jeg kan ændre i det";
+
     }
 }
